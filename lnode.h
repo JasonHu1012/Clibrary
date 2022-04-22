d7f0089650e55b5fd205af8845243a63e7fdf670
jasonhu <jasonhu1012@hotmail.com.tw>
2022-04-22 09:58:59 +0800

    move struct of lnode

--- lnode.h
+++ lnode.h
@@ -1,16 +1,15 @@
 #ifndef _LNODE_H
 #define _LNODE_H
 
-typedef struct lnode lnode;
+typedef struct lnode {
+    struct lnode *prev;
+    struct lnode *next;
+    void *content;
+} lnode;
 
 // let width = 0 to create dummy node
 lnode *lnd_init(void *src, int width);
 void lnd_kill(lnode *n);
 
-struct lnode {
-    struct lnode *prev;
-    struct lnode *next;
-    void *content;
-};
 
 #endif
