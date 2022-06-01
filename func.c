a627b07aecfaf0ec967aab243de87439a9f0bda1
jasonhu <jasonhu1012@hotmail.com.tw>
2022-06-01 16:15:14 +0800

    new lib: func

---
+++ func.c
@@ -0,0 +1,33 @@
+#include "func.h"
+#include <stdlib.h>
+#include <assert.h>
+
+void *salloc(int width, int *len, int dim) {
+    assert(dim > 0 && width > 0);
+    int total_len = 0;
+    int temp = len[0];
+    for (int i = 1; i < dim; i++) {
+        total_len += sizeof(void *) * temp;
+        temp *= len[i];
+    }
+    total_len += temp * width;
+    void *output = malloc(total_len);
+    if (dim == 1) {
+        return output;
+    }
+    void **cur = output;
+    char *target = (char *)&cur[len[0]];
+    temp = len[0];
+    for (int i = 1; i < dim - 1; i++) {
+        for (int j = 0; j < temp; j++) {
+            cur[j] = (void *)&target[sizeof(void *) * j * len[i]];
+        }
+        cur += temp;
+        temp *= len[i];
+        target += sizeof(void *) * temp;
+    }
+    for (int i = 0; i < temp; i++) {
+        cur[i] = (void *)&target[width * i * len[dim - 1]];
+    }
+    return output;
+}
---
+++ func.h
@@ -0,0 +1,6 @@
+#ifndef _FUNC_H
+#define _FUNC_H
+
+void *salloc(int width, int *len, int dim);
+
+#endif
