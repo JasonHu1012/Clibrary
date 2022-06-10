a393ed215684934006e862a53adb7125551b4d24
jasonhu <jasonhu1012@hotmail.com.tw>
2022-06-11 01:47:19 +0800

    put back lnode in the bottom, stupid

--- lnode.h
+++ lnode.h
@@ -1,14 +1,16 @@
 #ifndef _LNODE_H
 #define _LNODE_H
 
-typedef struct lnode {
-    lnode *prev;
-    lnode *next;
-    void *content;
-} lnode;
+typedef struct lnode lnode;
 
 // let width = 0 to create dummy node
 lnode *lnd_init(void *src, int width);
 void lnd_kill(lnode *n);
+
+struct lnode {
+    lnode *prev;
+    lnode *next;
+    void *content;
+};
 
 #endif
