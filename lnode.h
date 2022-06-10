88d3532037c3878a1c68b4545be1360e85fdebd6
jasonhu <jasonhu1012@hotmail.com.tw>
2022-06-10 22:25:37 +0800

    move lnode struct to top

--- lnode.h
+++ lnode.h
@@ -1,16 +1,14 @@
 #ifndef _LNODE_H
 #define _LNODE_H
 
-typedef struct lnode lnode;
+typedef struct lnode {
+    lnode *prev;
+    lnode *next;
+    void *content;
+} lnode;
 
 // let width = 0 to create dummy node
 lnode *lnd_init(void *src, int width);
 void lnd_kill(lnode *n);
-
-struct lnode {
-    lnode *prev;
-    lnode *next;
-    void *content;
-};
 
 #endif
