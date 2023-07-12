317f173c55c77bb5b3ea1807ea1d213f7fc7e349
jasonhu <jasonhu1012@hotmail.com.tw>
2023-07-12 20:19:04 +0800

    remove magic number in list test

--- test/list_test.c
+++ test/list_test.c
@@ -15,12 +15,14 @@ void test1() {
 }
 
 void test2() {
+    int size = 1000;
+
     printf("lst_set_size, lst_size... ");
 
     list *lst = lst_init(sizeof(int));
 
-    lst_set_size(lst, 1000);
-    assert(lst_size(lst) == 1000);
+    lst_set_size(lst, size);
+    assert(lst_size(lst) == size);
 
     lst_kill(lst);
 
@@ -41,20 +43,22 @@ void test3() {
 }
 
 void test4() {
+    int size = 1000;
+
     printf("lst_get, lst_set... ");
 
     list *lst = lst_init(sizeof(int));
-    lst_set_size(lst, 1000);
+    lst_set_size(lst, size);
 
-    for (int i = 0; i < 1000; i++) {
-        int n = i + 100;
+    for (int i = 0; i < size; i++) {
+        int n = i + size;
         lst_set(lst, i, &n);
-        assert(n == i + 100);
+        assert(n == i + size);
     }
-    for (int i = 999; i > -1; i--) {
+    for (int i = size - 1; i > -1; i--) {
         int n = -1;
         lst_get(lst, i, &n);
-        assert(n == i + 100);
+        assert(n == i + size);
     }
 
     lst_kill(lst);
@@ -63,20 +67,22 @@ void test4() {
 }
 
 void test5() {
+    int size = 1000;
+
     printf("lst_append, lst_pop... ");
 
     list *lst = lst_init(sizeof(int));
 
-    for (int i = 0; i < 1000; i++) {
-        int n = i + 100;
+    for (int i = 0; i < size; i++) {
+        int n = i + size;
         lst_append(lst, &n);
-        assert(n == i + 100);
+        assert(n == i + size);
         assert(lst_size(lst) == i + 1);
     }
-    for (int i = 999; i > -1; i--) {
+    for (int i = size - 1; i > -1; i--) {
         int n = -1;
         lst_pop(lst, &n);
-        assert(n == i + 100);
+        assert(n == i + size);
         assert(lst_size(lst) == i);
     }
 
@@ -89,7 +95,7 @@ void test6() {
     printf("lst_pop `dst` can be NULL... ");
 
     list *lst = lst_init(sizeof(int));
-    int n = 100;
+    int n;
     lst_append(lst, &n);
 
     lst_pop(lst, NULL);
@@ -100,36 +106,41 @@ void test6() {
 }
 
 void test7() {
+    int size = 100;
+    int T = 1000;
+    int index = 50;
+
     printf("lst_insert, lst_remove... ");
 
     list *lst = lst_init(sizeof(int));
-    lst_set_size(lst, 100);
+    lst_set_size(lst, size);
 
-    for (int i = 0; i < 1000; i++) {
-        int n = i + 100;
-        lst_insert(lst, 50, &n);
-        assert(n == i + 100);
-        assert(lst_size(lst) == 100 + i + 1);
+    for (int i = 0; i < T; i++) {
+        int n = i + size;
+        lst_insert(lst, index, &n);
+        assert(n == i + size);
+        assert(lst_size(lst) == size + i + 1);
     }
-    for (int i = 999; i > -1; i--) {
+    for (int i = T - 1; i > -1; i--) {
         int n = -1;
-        lst_remove(lst, 50, &n);
-        assert(n == i + 100);
-        assert(lst_size(lst) == 100 + i);
+        lst_remove(lst, index, &n);
+        assert(n == i + size);
+        assert(lst_size(lst) == size + i);
     }
 
     printf("pass\n");
 }
 
 void test8() {
+    int size = 100;
+    int index = 50;
+
     printf("lst_remove `dst` can be NULL... ");
 
     list *lst = lst_init(sizeof(int));
-    lst_set_size(lst, 100);
-    int n = 100;
-    lst_set(lst, 50, &n);
+    lst_set_size(lst, size);
 
-    lst_remove(lst, 50, NULL);
+    lst_remove(lst, index, NULL);
 
     lst_kill(lst);
 
@@ -137,20 +148,22 @@ void test8() {
 }
 
 void test9() {
+    int size = 10000;
     int T = 10000000;
+
     printf("%d lst_get and lst_set... ", T);
 
     list *lst = lst_init(sizeof(int));
-    lst_set_size(lst, 10000);
+    lst_set_size(lst, size);
     int n;
     clock_t start = clock();
 
     for (int i = 0; i < T; i++) {
-        n = rand() % 100;
-        lst_set(lst, rand() % 10000, &n);
+        n = rand();
+        lst_set(lst, rand() % size, &n);
     }
     for (int i = 0; i < T; i++) {
-        lst_get(lst, rand() % 10000, &n);
+        lst_get(lst, rand() % size, &n);
     }
 
     clock_t end = clock();
